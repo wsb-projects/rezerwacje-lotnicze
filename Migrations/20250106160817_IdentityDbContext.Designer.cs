b5fb56fd91f64cb5b2e1a594f3d64ba8c368b22f
dominik <dominik0kielkowski@gmail.com>
2025-01-09 19:24:38 +0100

    Basic Identity implementation

--- Migrations/20250106160817_IdentityDbContext.Designer.cs
+++ Migrations/20250106160817_IdentityDbContext.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using rezerwacje_lotnicze.Infrastructure;
@@ -11,9 +12,11 @@ using rezerwacje_lotnicze.Infrastructure;
 namespace rezerwacje_lotnicze.Migrations
 {
     [DbContext(typeof(FlightBookingDbContext))]
-    partial class FlightBookingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250106160817_IdentityDbContext")]
+    partial class IdentityDbContext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+using rezerwacje_lotnicze.Infrastructure;
 using rezerwacje_lotnicze.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication().AddCookie(IdentityConstants.ApplicationScheme);
+
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddEntityFrameworkStores<FlightBookingDbContext>()
+    .AddApiEndpoints();
+
 builder.Services.AddFlightBookingDbContext(builder.Configuration);
 
 var app = builder.Build();
@@ -16,5 +26,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapIdentityApi<IdentityUser>();
 
 app.Run();
