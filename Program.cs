d5c134dd11c93a0d9561b89b331dd941bb0841a6
void404 <9433601+VOID404@users.noreply.github.com>
2025-01-13 09:31:45 +0100

    Wire `User` to auth

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Identity;
 using rezerwacje_lotnicze.Application.Interfaces;
 using rezerwacje_lotnicze.Application.Services;
+using rezerwacje_lotnicze.Domain.Entities.User;
 using rezerwacje_lotnicze.Infrastructure;
 using rezerwacje_lotnicze.Infrastructure.Extensions;
 using rezerwacje_lotnicze.Infrastructure.Seeders;
@@ -12,12 +14,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IFlightService, FlightService>();
+builder.Services.AddScoped<ITicketService, TicketService>();
 builder.Services.AddScoped<FlightSeeder>();
 
 builder.Services.AddAuthorization();
 builder.Services.AddAuthentication().AddCookie(IdentityConstants.ApplicationScheme);
 
-builder.Services.AddIdentityCore<IdentityUser>()
+builder.Services.AddIdentityCore<User>()
     .AddEntityFrameworkStores<FlightBookingDbContext>()
     .AddApiEndpoints();
 
@@ -38,6 +41,6 @@ app.UseHttpsRedirection();
 
 app.MapControllers();
 
-app.MapIdentityApi<IdentityUser>();
+app.MapIdentityApi<User>();
 
 app.Run();
