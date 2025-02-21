d700e29e37319fc25e13fb397bd43b0b4b07685d
Dominik-Kielkowski <dominik0kielkowski@gmail.com>
2025-02-21 18:01:42 +0100

    Expand IdentityExtensions.cs

--- Infrastructure/Extensions/IdentityExtensions.cs
+++ Infrastructure/Extensions/IdentityExtensions.cs
@@ -1,5 +1,10 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using rezerwacje_lotnicze.Application.Interfaces;
 using rezerwacje_lotnicze.Domain.Entities.User;
+using rezerwacje_lotnicze.Infrastructure.Identity;
 
 namespace rezerwacje_lotnicze.Infrastructure.Extensions;
 
@@ -7,13 +12,32 @@ public static class IdentityExtensions
 {
     public static IServiceCollection AddIdentityServices(this IServiceCollection services)
     {
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IAuthService, AuthService>();
+
         services.AddIdentityCore<User>()
             .AddRoles<IdentityRole>()
-            .AddEntityFrameworkStores<FlightBookingDbContext>()
-            .AddApiEndpoints();
+            .AddEntityFrameworkStores<FlightBookingDbContext>();
 
-        services.AddAuthentication().AddCookie(IdentityConstants.ApplicationScheme);
-        
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = "http://localhost:8080",
+                    ValidAudience = "http://localhost:8080",
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("yourSecretKey")),
+                    ClockSkew = TimeSpan.Zero
+                };
+                
+                options.RequireHttpsMetadata = true;
+                options.SaveToken = true;
+            });
+
         services.AddAuthorization();
 
         return services;
@@ -21,6 +45,8 @@ public static class IdentityExtensions
     
     public static void MapIdentityRoutes(this WebApplication app)
     {
+        app.UseAuthentication();
+        app.UseAuthorization();
         app.MapIdentityApi<User>();
     }
 }
--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.MapControllers();
 app.MapIdentityRoutes();
+app.MapControllers();
 
 app.Run();
